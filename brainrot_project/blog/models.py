67409528869cae5eaa7e2fa90856176ac15ee7bd
BejenaruIoanMatei <bejematei@gmail.com>
2025-04-19 19:05:25 +0300

    2. Blog App: Post model done

--- brainrot_project/blog/models.py
+++ brainrot_project/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from PIL import Image, UnidentifiedImageError
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to='post_images/')
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        try:
+            img = Image.open(self.image.path)
+            
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+            
+            if img.height > 400 or img.width > 400:
+                output_size = (400,400)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
+
+        except UnidentifiedImageError:
+            print(f"Could not identify image at {self.image.path}")
