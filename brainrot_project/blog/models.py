bec799c1e503a49eb32dde3292442319310d97dd
BejenaruIoanMatei <bejematei@gmail.com>
2025-04-27 16:15:17 +0300

    6: Users: manage posts and profiles

--- brainrot_project/blog/models.py
+++ brainrot_project/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from PIL import Image, UnidentifiedImageError
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -28,3 +30,7 @@ class Post(models.Model):
 
         except UnidentifiedImageError:
             print(f"Could not identify image at {self.image.path}")
+            
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+        
